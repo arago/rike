bae460c1c45fb4035136a6acc1d9f6d0621b70e1
pitz <pitz@arago.de>
2012-08-17 18:49:56 +0200

    add debug util

---
+++ arago-debug-util/src/main/java/de/arago/debug/performance/Performance.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2010 arago AG, http://www.arago.de/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.arago.debug.performance;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public final class Performance
+{
+  private static final Logger log = Logger.getLogger(Performance.class.getName());
+  
+  private static final boolean enabled;
+  private static final String prefix;
+  private static final String configPrefix = "de.arago.debug.perf.";
+  
+  private static final StatsdClient client;
+  
+  static
+  {
+    final String host = System.getProperty(configPrefix + "host", "");
+    
+    if (host != null && !host.isEmpty())
+    {
+      prefix  = System.getProperty(configPrefix + "prefix", "");
+      int port = 8125;
+    
+      try
+      {
+        port = Integer.valueOf(System.getProperty(configPrefix + "port", "8125"), 10);
+      } catch(Exception e) {
+        // blank
+      }
+      
+      StatsdClient tmpClient = null;
+    
+      try 
+      {
+        tmpClient = new StatsdClient(host, port);
+      } catch (Exception ex) {
+        log.log(Level.SEVERE, null, ex);
+      }
+
+    
+      client  = tmpClient;
+      enabled = client != null;
+      
+      if (enabled) log.log(Level.INFO, "{Performance} statistics will be gathered on " + host + ":" + port);
+    } else {
+      client  = null;
+      enabled = false;
+      prefix  = null;
+    }  
+  }      
+  
+  /**
+   * increment a key
+   * @param key 
+   */
+  public static void increment(String key)
+  {
+    if (enabled) client.increment(prefix + key);
+  }  
+  
+  
+  /**
+   * decrement a key
+   * @param key 
+   */
+  public static void decrement(String key)
+  {
+    if (enabled) client.decrement(prefix + key);
+  }
+  
+  /**
+   * time some operation
+   * 
+   * @param key
+   * @param howLong 
+   */
+  public static void timing(String key, long howLong)
+  {
+    if (!enabled) return;
+            
+    int t;
+    
+    if (howLong < Integer.MIN_VALUE) 
+    {
+      t = Integer.MIN_VALUE;
+    } else if (howLong > Integer.MAX_VALUE) {
+      t = Integer.MAX_VALUE;
+    } else {
+      t = (int) howLong;
+    }
+    
+    client.timing(prefix + key, t);
+  }      
+  
+  public static boolean isEnabled()
+  {
+    return enabled;
+  }          
+}
---
+++ arago-debug-util/src/main/java/de/arago/debug/performance/StatsdClient.java
@@ -0,0 +1,180 @@
+
+package de.arago.debug.performance;
+
+/**
+ * StatsdClient.java
+ *
+ * (C) 2011 Meetup, Inc. Author: Andrew Gwozdziewycz <andrew@meetup.com>,
+ * @apgwoz
+ *
+ *
+ *
+ * Example usage:
+ *
+ * StatsdClient client = new StatsdClient("statsd.example.com", 8125); //
+ * increment by 1 client.increment("foo.bar.baz"); // increment by 10
+ * client.increment("foo.bar.baz", 10); // sample rate
+ * client.increment("foo.bar.baz", 10, .1); // increment multiple keys by 1
+ * client.increment("foo.bar.baz", "foo.bar.boo", "foo.baz.bar"); // increment
+ * multiple keys by 10 -- yeah, it's "backwards" client.increment(10,
+ * "foo.bar.baz", "foo.bar.boo", "foo.baz.bar"); // multiple keys with a sample
+ * rate client.increment(10, .1, "foo.bar.baz", "foo.bar.boo", "foo.baz.bar");
+ *
+ * Note: For best results, and greater availability, you'll probably want to
+ * create a wrapper class which creates a static client and proxies to it.
+ *
+ * You know... the "Java way."
+ *
+ * @license
+ * https://github.com/etsy/statsd/blob/576f977e30cbac6475e4615223422661c3ddc486/LICENSE
+ */
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StatsdClient
+{
+
+  private static Random RNG = new Random();
+  private static Logger log = Logger.getLogger(StatsdClient.class.getName());
+  private InetSocketAddress _address;
+  private DatagramChannel _channel;
+
+  public StatsdClient(String host, int port) throws UnknownHostException, IOException
+  {
+    this(InetAddress.getByName(host), port);
+  }
+
+  public StatsdClient(InetAddress host, int port) throws IOException
+  {
+    _address = new InetSocketAddress(host, port);
+    _channel = DatagramChannel.open();
+  }
+
+  public boolean timing(String key, int value)
+  {
+    return timing(key, value, 1.0);
+  }
+
+  public boolean timing(String key, int value, double sampleRate)
+  {
+    return send(sampleRate, String.format("%s:%d|ms", key, value));
+  }
+
+  public boolean decrement(String key)
+  {
+    return increment(key, -1, 1.0);
+  }
+
+  public boolean decrement(String key, int magnitude)
+  {
+    return decrement(key, magnitude, 1.0);
+  }
+
+  public boolean decrement(String key, int magnitude, double sampleRate)
+  {
+    magnitude = magnitude < 0 ? magnitude : -magnitude;
+    return increment(key, magnitude, sampleRate);
+  }
+
+  public boolean decrement(String... keys)
+  {
+    return increment(-1, 1.0, keys);
+  }
+
+  public boolean decrement(int magnitude, String... keys)
+  {
+    magnitude = magnitude < 0 ? magnitude : -magnitude;
+    return increment(magnitude, 1.0, keys);
+  }
+
+  public boolean decrement(int magnitude, double sampleRate, String... keys)
+  {
+    magnitude = magnitude < 0 ? magnitude : -magnitude;
+    return increment(magnitude, sampleRate, keys);
+  }
+
+  public boolean increment(String key)
+  {
+    return increment(key, 1, 1.0);
+  }
+
+  public boolean increment(String key, int magnitude)
+  {
+    return increment(key, magnitude, 1.0);
+  }
+
+  public boolean increment(String key, int magnitude, double sampleRate)
+  {
+    String stat = String.format("%s:%s|c", key, magnitude);
+    return send(stat, sampleRate);
+  }
+
+  public boolean increment(int magnitude, double sampleRate, String... keys)
+  {
+    String[] stats = new String[keys.length];
+    for (int i = 0; i < keys.length; i++) {
+      stats[i] = String.format("%s:%s|c", keys[i], magnitude);
+    }
+    return send(sampleRate, stats);
+  }
+
+  private boolean send(String stat, double sampleRate)
+  {
+    return send(sampleRate, stat);
+  }
+
+  private boolean send(double sampleRate, String... stats)
+  {
+
+    boolean retval = false; // didn't send anything
+    if (sampleRate < 1.0) {
+      for (String stat: stats) {
+        if (RNG.nextDouble() <= sampleRate) {
+          stat = String.format("%s|@%f", stat, sampleRate);
+          if (doSend(stat)) {
+            retval = true;
+          }
+        }
+      }
+    } else {
+      for (String stat: stats) {
+        if (doSend(stat)) {
+          retval = true;
+        }
+      }
+    }
+
+    return retval;
+  }
+
+  private boolean doSend(final String stat)
+  {
+    try {
+      final byte[] data = stat.getBytes("utf-8");
+      final ByteBuffer buff = ByteBuffer.wrap(data);
+      final int nbSentBytes = _channel.send(buff, _address);
+
+      if (data.length == nbSentBytes) {
+        return true;
+      } else {
+        log.log(Level.SEVERE, String.format(
+                "Could not send entirely stat %s to host %s:%d. Only sent %i bytes out of %i bytes", stat,
+                _address.getHostName(), _address.getPort(), nbSentBytes, data.length));
+        return false;
+      }
+
+    } catch (IOException e) {
+      log.log(Level.SEVERE,
+              String.format("Could not send stat %s to host %s:%d", stat, _address.getHostName(),
+              _address.getPort()), e);
+      return false;
+    }
+  }
+}
