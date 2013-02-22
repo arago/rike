2fc5f9429bd972b4007e0a8b3d57cd030e77749d
viteke <vvoss@voss.devel.arago.de>
2013-02-22 16:43:17 +0100

    add arago-lucene

---
+++ arago-lucene/src/main/java/de/arago/lucene/util/IndexUpdater.java
@@ -0,0 +1,13 @@
+package de.arago.lucene.util;
+
+import de.arago.lucene.api.Index;
+
+abstract public class IndexUpdater<T>
+{
+  /**
+   * update one item in the index
+   * @param index
+   * @param item 
+   */
+	abstract public void update(Index<T> index, Object item);
+}
