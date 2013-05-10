a545bc8549bc0e838013b36197570095464933d4
pitz <pitz@arago.de>
2013-05-10 19:14:22 +0200

    fixes, new features

---
+++ arago-rike/src/main/java/de/arago/rike/zombie/Zombies.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2010 arago AG, http://www.arago.de/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.arago.rike.zombie;
+
+import de.arago.rike.task.*;
+import de.arago.portlet.AragoPortlet;
+import de.arago.portlet.util.SecurityHelper;
+
+import de.arago.data.IDataWrapper;
+
+import java.io.IOException;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+
+public class Zombies extends AragoPortlet {
+
+    @Override
+    public void init(PortletConfig config) throws PortletException {
+
+        super.init(config);
+    }
+
+    @Override
+    public void initSession(IDataWrapper data) throws PortletException, IOException {
+        if (!checkViewData(data)) {
+            return;
+        }
+    }
+
+    @Override
+    protected boolean checkViewData(IDataWrapper data) {
+        data.setSessionAttribute("userEmail", SecurityHelper.getUserEmail(data.getUser()));
+        return SecurityHelper.isLoggedIn(data.getUser());
+    }
+}
