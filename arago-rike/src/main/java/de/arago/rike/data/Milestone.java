f2f509ff655c01c16e72d669cdb9c35b7799b66c
vvoss <vvoss@192.168.1.101>
2013-05-15 02:28:09 +0200

    add simple svg

--- arago-rike/src/main/java/de/arago/rike/data/Milestone.java
+++ arago-rike/src/main/java/de/arago/rike/data/Milestone.java
@@ -1,24 +1,18 @@
 /**
  * Copyright (c) 2010 arago AG, http://www.arago.de/
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package de.arago.rike.data;
 
@@ -31,10 +25,9 @@ public class Milestone {
     private Date dueDate;
     private String release;
     private String url;
-
     private String creator;
     private Date created;
-  private Integer performance;
+    private Integer performance;
 
     public void setId(Long id) {
         this.id = id;
@@ -102,13 +95,19 @@ public class Milestone {
         return created;
     }
 
-  public void setPerformance(Integer what)
-  {
-    this.performance = what;
-  }
-  
-  public int getPerformance()
-  {
-    return performance;
-  }  
+    public void setPerformance(Integer what) {
+        this.performance = what;
+    }
+
+    public int getPerformance() {
+        return performance;
+    }
+    
+    public long getDays() {
+        long d = 0;
+        if(getDueDate()!=null)
+            d = getDueDate().getTime();
+        return (d-System.currentTimeMillis())/(1000*60*60);
+    }
+
 }
--- arago-rike/src/main/java/de/arago/rike/zombie/ZombieHelper.java
+++ arago-rike/src/main/java/de/arago/rike/zombie/ZombieHelper.java
@@ -7,6 +7,7 @@ package de.arago.rike.zombie;
 import de.arago.rike.data.DataHelperRike;
 import de.arago.rike.data.Milestone;
 import de.arago.rike.data.Task;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -17,43 +18,42 @@ import org.hibernate.criterion.Restrictions;
 /**
  *
  */
-public class ZombieHelper
-{
-  public static List<Milestone> getOverdueMilestones()
-  {
-    DataHelperRike<Milestone> helper = new DataHelperRike<Milestone>(Milestone.class);
+public class ZombieHelper {
+
+    public static List<Milestone> getOverdueMilestones() {
+        DataHelperRike<Milestone> helper = new DataHelperRike<Milestone>(Milestone.class);
+
+        String str = "select id, (select sum(size_estimated) from tasks t where t.milestone_id = m.id and t.task_status != 'done') as hours_left from milestones m group by m.id having hours_left is not null;";
+
+        System.err.println("querying");
+        List<Milestone> list = helper.list();
+        List<Milestone> result = new ArrayList<Milestone>();
+
+        for (final Milestone o : list) {
+            System.err.println(o.getTitle()+" "+o.getDays());
+            if(o.getDays()>0)
+                result.add(o);
+        }
+        /*Criteria crit = helper.filter()
+         .add(Restrictions.isNotNull("dueDate"))
+         .add(Restrictions.lt("dueDate", new Date()))
+         .add(Restrictions.ne("status", Task.Status.DONE.toString().toLowerCase()))
+         .addOrder(Order.asc("dueDate"));
     
-    String str = "select id, (select sum(size_estimated) from tasks t where t.milestone_id = m.id and t.task_status != 'done') as hours_left from milestones m group by m.id having hours_left is not null;";
-    
-    System.err.println("querying");
-    
-      List<Object> list = helper.list(helper.createSQLQuery(str));
-      
-      for (final Object o: list)
-      {
-        System.err.println(o);
-      }  
-    /*Criteria crit = helper.filter()
-    .add(Restrictions.isNotNull("dueDate"))
-    .add(Restrictions.lt("dueDate", new Date()))
-    .add(Restrictions.ne("status", Task.Status.DONE.toString().toLowerCase()))
-    .addOrder(Order.asc("dueDate"));
-    
-    return helper.list(crit);*/
-    
-    return Collections.EMPTY_LIST;
-  }  
-  
-  public static List<Task> getOverdueTasks()
-  {
-    DataHelperRike<Task> helper = new DataHelperRike<Task>(Task.class);
-    Criteria crit = helper.filter()
-                          .add(Restrictions.isNotNull("dueDate"))
-                          .add(Restrictions.lt("dueDate", new Date()))
-                          .add(Restrictions.ne("status", Task.Status.DONE.toString().toLowerCase()))
-                          .addOrder(Order.asc("dueDate"));
-    
-    
-    return helper.list(crit);
-  }  
+         return helper.list(crit);*/
+
+        return result;
+    }
+
+    public static List<Task> getOverdueTasks() {
+        DataHelperRike<Task> helper = new DataHelperRike<Task>(Task.class);
+        Criteria crit = helper.filter()
+                .add(Restrictions.isNotNull("dueDate"))
+                .add(Restrictions.lt("dueDate", new Date()))
+                .add(Restrictions.ne("status", Task.Status.DONE.toString().toLowerCase()))
+                .addOrder(Order.asc("dueDate"));
+
+
+        return helper.list(crit);
+    }
 }
