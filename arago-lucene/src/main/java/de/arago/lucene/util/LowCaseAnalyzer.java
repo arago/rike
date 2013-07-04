013e249faaef46c6bfec416ec853235b8b811f9a
pitz <pitz@arago.de>
2013-07-04 16:10:43 +0200

    allow only alphanum in ngrams

--- arago-lucene/src/main/java/de/arago/lucene/util/LowCaseAnalyzer.java
+++ arago-lucene/src/main/java/de/arago/lucene/util/LowCaseAnalyzer.java
@@ -4,9 +4,12 @@ import java.io.Reader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LetterTokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
 import org.apache.lucene.analysis.ngram.NGramTokenizer;
+import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.util.Version;
 
 /**
@@ -33,13 +36,13 @@ final public class LowCaseAnalyzer extends Analyzer {
       
     if("_name_prefix".equals(fieldName)) {
         source = new NGramTokenizer(reader,mingram,maxgram);
-        stream = new LowerCaseFilter(Version.LUCENE_43, source);
+        stream = new WordDelimiterFilter(new LowerCaseFilter(Version.LUCENE_43,source), WordDelimiterFilter.ALPHANUM, CharArraySet.EMPTY_SET);
     } else if(fieldName.startsWith("_ngram_")) {
         source = new NGramTokenizer(reader,3,4);
         stream = null;
     } else {
-        source = new WhitespaceTokenizer(Version.LUCENE_36,reader);
-        stream = new LowerCaseFilter(Version.LUCENE_43,source);
+        source = new WhitespaceTokenizer(Version.LUCENE_43,reader);
+        stream = new WordDelimiterFilter(new LowerCaseFilter(Version.LUCENE_43,source), WordDelimiterFilter.ALPHANUM, CharArraySet.EMPTY_SET);
     }
     
     return new TokenStreamComponents(source, stream);
